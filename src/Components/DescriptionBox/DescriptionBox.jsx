8938ca50c85684fab4c874d5c8ea1d674368c6c8
Asmin Shrestha <asmin.stha77@gmail.com>
2025-03-07 21:55:23 +0545

    Cart Page added

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './DescriptionBox.css'
+const DescriptionBox=()=>{
+  return (
+<div className="descriptionbox">
+
+<div className="descriptionbox-navigator">
+    <div className="descriptionbox-nav-box">
+        Description
+    </div>
+    <div className="descriptionbox-nav-box fade">
+        Reviews (122)
+    </div>
+</div>
+<div className="descriptionbox-description">
+    <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis quod dolorum debitis suscipit expedita, dolore repellendus voluptates quos placeat labore voluptatum ab quibusdam asperiores ipsum vel eaque odio cum aut.
+        Rerum harum quidem nemo optio dolore amet sed mollitia totam voluptate, error incidunt, ad deserunt corrupti, praesentium sunt unde eaque reiciendis debitis nam dicta numquam tenetur molestias quasi quod. Quibusdam.
+    </p>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur tempore adipisci consequuntur qui deleniti reiciendis, cupiditate eum illum quia officia ipsa fuga atque voluptate esse vero libero dolorum? Magnam, fuga.z
+    </p>
+</div>
+</div>
+
+
+  )
+}
+
+export default DescriptionBox
